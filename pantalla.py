8885e3a5d0cde71f18598c25430399ebb31594a4
diego226-afk <dianconafer@gmail.com>
2024-11-18 18:46:21 +0100

    new file:   azul.pyxres modified:   pantalla.py

--- pantalla.py
+++ pantalla.py
@@ -1,12 +1,30 @@
 import pyxel
-pyxel.init(160, 120)
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)  
+        pyxel.load("azul.pyxres")
+        self.x = 0
+        self.y = 0
+        self.vel=2
+        pyxel.run(self.update, self.draw)
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
+    def update(self):
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.x + 16 < pyxel.width:
+            self.x += self.vel 
+        if pyxel.btn(pyxel.KEY_LEFT) and self.x > 0:
+            self.x -= self.vel 
+        if pyxel.btn(pyxel.KEY_UP) and self.y > 0:
+            self.y -= self.vel 
+        if pyxel.btn(pyxel.KEY_DOWN) and self.y + 16 < pyxel.height:
+            self.y += self.vel 
 
-pyxel.run(update, draw)
+        
+        
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.blt(self.x, self.y, 0, 0, 0, 14, 16, 0)
+
+App()
+
