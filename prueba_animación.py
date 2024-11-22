10a4c27f6bc75563c142f63d7ccee3b7872506ad
diego226-afk <dianconafer@gmail.com>
2024-11-22 17:28:49 +0100

    arreglado, pacman no se sale de la imagen

--- prueba_animación.py
+++ prueba_animación.py
@@ -10,19 +10,22 @@ class Pacman:
         self.direccion = "derecha"
         self.temporizador_animacion = 0
         self.sprite_actual = 0
+        self.base=1
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT) and self.x - self.velocidad >= 0:
+        if (pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)) and (self.x - self.velocidad >= 0):
             self.x -= self.velocidad
             self.direccion = "izquierda"
-        if pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT) and self.x + self.velocidad <= pyxel.width - 15:
+            self.base=1
+        if (pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)) and (self.x + self.velocidad <= pyxel.width - 15):
             self.x += self.velocidad
             self.direccion = "derecha"
-        if pyxel.btn(pyxel.KEY_W) or pyxel.btn(pyxel.KEY_UP) and self.y - self.velocidad >= 0:
+            self.base=-1
+        if (pyxel.btn(pyxel.KEY_W) or pyxel.btn(pyxel.KEY_UP)) and (self.y - self.velocidad >= 0):
             self.y -= self.velocidad
             self.direccion = "arriba"
-        if pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_DOWN) and self.y + self.velocidad <= pyxel.height - 16:
+        if (pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_DOWN)) and (self.y + self.velocidad <= pyxel.height - 16):
             self.y += self.velocidad
             self.direccion = "abajo"
         
@@ -36,13 +39,13 @@ class Pacman:
 
         # Ajustar los parámetros del sprite para cada dirección
         if self.direccion == "derecha":
-            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, -15, 16, 0)
+            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, self.base*15, 16, 0)
         elif self.direccion == "izquierda":
-            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, 15, 16, 0)
+            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, self.base*15, 16, 0)
         elif self.direccion == "arriba":
-            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, 15, 16, 0)
+            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, self.base*15, 16, 0)
         elif self.direccion == "abajo":
-            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, 15, 16, 0)
+            pyxel.blt(self.x, self.y, 0, self.sprite_actual * 15, 0, self.base*15, 16, 0)
 
 Pacman()
 
