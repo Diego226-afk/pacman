f783b43847d332625bd1cbd65424789a45d0033d
jabbub <jmorafr@gmail.com>
2024-11-22 15:06:23 +0100

    ya funciona clase_pacman, el problema de las imgenes lo podemos dejar para el final, que tampoco hay que perder mucho tiempo en eso

--- clase_pacman.py
+++ clase_pacman.py
@@ -1,29 +1,30 @@
 import pyxel
 
 class Pacman:
-    def __init__(self, x, y):
+    def __init__(self):
         
         pyxel.init(256, 256, title="Pacman")  
-        
-        self.x = x  
-        self.y = y  
-        self.velocidad = 5  
+        self.x = 0  
+        self.y = 0  
+
         self.direccion = "derecha"
         pyxel.images[0].load(0, 0, "boca-abierta.png")
         pyxel.images[1].load(0, 0, "boca-cerrada.png")
 
+        pyxel.run(self.update, self.draw)
+
     def update(self):
         if pyxel.btn(pyxel.KEY_A):
-            self.x -= self.velocidad
+            self.x -= 2
             self.direccion = "izquierda"
         if pyxel.btn(pyxel.KEY_D):
-            self.x += self.velocidad
+            self.x += 2
             self.direccion = "derecha"
         if pyxel.btn(pyxel.KEY_W):
-            self.y -= self.velocidad
+            self.y -= 2
             self.direccion = "arriba"
         if pyxel.btn(pyxel.KEY_S):
-            self.y += self.velocidad
+            self.y += 2
             self.direccion = "abajo"
 
     def draw(self):
@@ -38,5 +39,4 @@ class Pacman:
         elif self.direccion == "abajo":
             pyxel.blt(self.x, self.y, 1, 0, 0, 17, 17, 0) 
             
-player = Pacman(128, 128)
 Pacman()
