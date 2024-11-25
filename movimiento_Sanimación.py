ca3b94794ca0b431eab0755e1f88547be4cc7fd1
diego226-afk <dianconafer@gmail.com>
2024-11-25 15:55:10 +0100

    new file:   bloque.pyxres modified:   "movimiento_Sanimaci\303\263n.py"

--- movimiento_Sanimación.py
+++ movimiento_Sanimación.py
@@ -10,26 +10,28 @@ class Pacman:
         self.direccion = "derecha"
         self.temporizador_animacion = 0
         self.sprite_actual = 0
+        self.base=1
+        self.arriba=1
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        # Movimiento y cambio de dirección
         if (pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)) and (self.x - self.velocidad >= 0):
             self.x -= self.velocidad
             self.direccion = "izquierda"
-            self.base = 1
+            self.base = -1
         if (pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)) and (self.x + self.velocidad <= pyxel.width - 15):
             self.x += self.velocidad
             self.direccion = "derecha"
-            self.base = -1
+            self.base = 1
         if (pyxel.btn(pyxel.KEY_W) or pyxel.btn(pyxel.KEY_UP)) and (self.y - self.velocidad >= 0):
             self.y -= self.velocidad
             self.direccion = "arriba"
+            self.base = 1
         if (pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_DOWN)) and (self.y + self.velocidad <= pyxel.height - 16):
             self.y += self.velocidad
             self.direccion = "abajo"
-        
-        # Comprobar teletransporte
+            self.base = -1
+
         self.tp()
 
     def tp(self):
@@ -38,9 +40,13 @@ class Pacman:
             self.y=128
 
     def draw(self):
-        pyxel.cls(0)  # Limpiar pantalla
-        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 17, 0)
+        pyxel.cls(0)
+        if self.direccion == "derecha" or self.direccion == "izquierda":
+            pyxel.blt(self.x, self.y, 0, 0, 0, self.base*16, 17, 0)
+        if self.direccion == "arriba" or self.direccion == "abajo":
+            pyxel.blt(self.x, self.y, 0, 0, 0, 16, self.base*17, 0)
+        
 
-class Fant:
+class map:
     pass
 Pacman()
